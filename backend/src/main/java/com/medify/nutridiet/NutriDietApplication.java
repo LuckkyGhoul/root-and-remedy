1265e348ed76259cacee50ea082cb8c34b3542e4
Shashaank Mishra <plan06072005@gmail.com>
2026-06-29 18:05:19 +0530

    Upload backend/src/main/java/com/medify/nutridiet/NutriDietApplication.java via API script

---
+++ backend/src/main/java/com/medify/nutridiet/NutriDietApplication.java
@@ -0,0 +1,11 @@
+package com.medify.nutridiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriDietApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NutriDietApplication.class, args);
+    }
+}
